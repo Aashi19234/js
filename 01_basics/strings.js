4781d0591edd237f4ce2baf75172f2c578556d8c
Aashi19234 <140010647+Aashi19234@users.noreply.github.com>
2023-11-24 19:00:28 +0000

    done for the day

---
+++ 01_basics/strings.js
@@ -0,0 +1,11 @@
+const name="Aastha"
+const repoCount=50
+//console.log(`My name is ${name} and my repo count is ${repoCount}`);
+const gamename=new String('Aasthacool')//another way of declaring strings
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(2));
+console.log(gamename.indexOf('s'));
+
