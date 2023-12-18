9940b3287bb0683ac50ab4ce090bdb35f9ef68ad
Aashi19234 <aasthaj375@gmail.com>
2023-12-18 20:44:12 +0530

    till async

---
+++ 01_basics/async.js
@@ -0,0 +1,26 @@
+// imp for interview purposes
+
+// async-> asynchrous code
+// js is a synchronous language.
+// it is a single threaded langauage// these both are default js behaviour
+/*
+execution context: execute one line of code at a time
+jb tk purana stament execute ni hga tb tk naya ni hga
+
+ */
+
+/*
+block code vs non blocking code
+1)block code blocks the flow of program
+2) it reads file sync which means jb tk file read hri h tb tk koi aur kaam ni hga
+
+
+1)non blocking code does nt block executiom
+2)it reads file async jb tk file read hri h tb tk aur kaam bhi ho skta h
+
+// refer to ss 59 for the diagram
+
+
+
+
+*/
