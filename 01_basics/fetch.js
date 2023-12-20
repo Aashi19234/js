c1c96539fc425c3444d6e5c8b614406fdd4745d9
Aashi19234 <aasthaj375@gmail.com>
2023-12-20 23:53:17 +0530

    fetch

--- 01_basics/fetch.js
+++ 01_basics/fetch.js
@@ -1,1 +1,3 @@
-//
+// a fetch() promise only rejects when a network error is encountered(which is ususally when there is permission issue or similar )
+// refer to mdn web docs for fetch
+// also refer to ss 60 and 61  for the diagram
